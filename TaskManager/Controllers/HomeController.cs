29beca02bcbcefbc735fa3a97e636bd47d28b781
BayraktarD <108873101+BayraktarD@users.noreply.github.com>
2023-01-10 17:54:23 +0200

    ups...

--- TaskManager/Controllers/HomeController.cs
+++ TaskManager/Controllers/HomeController.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TaskManager.Models;
 
+//
 namespace TaskManager.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
+        
+
 
         public IActionResult Index()
         {
